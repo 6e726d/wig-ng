d675de316cc1decdcc05ca337dd321a3de89a0fa
6e726d <6e726d@gmail.com>
2022-04-01 11:22:37 -0300

    Updated copyright year.

--- helpers/ccx.py
+++ helpers/ccx.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # wig-ng - Wireless Information Gathering New Generation
-# Copyright (C) 2019 - Andrés Blanco (6e726d) <6e726d@gmail.com>
+# Copyright (C) 2022 - Andrés Blanco (6e726d) <6e726d@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
